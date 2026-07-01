b17768101bc887a9c53b320c21209eaabd5b63ca
Felipe Pesantez <felipepesantez@192.168.1.108>
2026-07-01 10:31:54 +0100

    Added GI working on Mac

---
+++ cmake/CMakeFiles/pbr_viewer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pbr_viewer.dir/glad/src/glad.c.o"
+  "CMakeFiles/pbr_viewer.dir/glad/src/glad.c.o.d"
+  "CMakeFiles/pbr_viewer.dir/main.cpp.o"
+  "CMakeFiles/pbr_viewer.dir/main.cpp.o.d"
+  "pbr_viewer"
+  "pbr_viewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/pbr_viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
